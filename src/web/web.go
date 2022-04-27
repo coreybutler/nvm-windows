6f62edb497f488839d06240414ced199ae7e6863
Todd Ogin <ojintoad@gmail.com>
2022-04-27 00:40:32 -0400

    revert fsutil issue

--- src/web/web.go
+++ src/web/web.go
@@ -169,7 +169,7 @@ func GetNodeJS(root string, v string, a string, append bool) bool {
 				}
 
 				zip := root + "\\v" + v + "\\" + strings.Replace(filepath.Base(url), ".zip", "", 1)
-				err = fs.Move(zip, root+"\\v"+v)
+				err = fs.Move(zip, root+"\\v"+v, true)
 				if err != nil {
 					fmt.Println("ERROR moving file: " + err.Error())
 				}
