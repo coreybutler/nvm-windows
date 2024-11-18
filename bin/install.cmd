5eb5bf9189fc3cba7c37259558814f9aba801013
Corey Butler <770982+coreybutler@users.noreply.github.com>
2024-11-18 12:13:29 -0600

    Attempt to enforce utf-8 encoding on settings file. Fixes #726.

--- bin/install.cmd
+++ bin/install.cmd
@@ -11,8 +11,6 @@ for /f "skip=2 tokens=2,*" %%A in ('reg query "HKLM\System\CurrentControlSet\Con
   setx /M PATH "%%B;%%NVM_HOME%%;%%NVM_SYMLINK%%"
 )
 
-chcp 65001 > nul
-
 if exist "%SYSTEMDRIVE%\Program Files (x86)\" (
 set SYS_ARCH=64
 ) else (
@@ -21,5 +19,4 @@ set SYS_ARCH=32
 (echo root: %NVM_HOME% && echo path: %NVM_SYMLINK% && echo arch: %SYS_ARCH% && echo proxy: none) > %NVM_HOME%\settings.txt
 
 notepad %NVM_HOME%\settings.txt
-
 @echo on
